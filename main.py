519417de5a3643514fef82a3efd7ec4486de03d4
bcrew1375 <no-reply>
2018-10-17 06:10:36 -0400

    Code refactoring
    Some code refactoring and food appearance issue should be resolved now.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pygame
 import random
 
-BOARD_SIZE_X = 25
-BOARD_SIZE_Y = 25
+BOARD_SIZE_WIDTH = 25
+BOARD_SIZE_HEIGHT = 25
+
+# This will determine the size of the objects drawn on the board.
+# Must be equal to resolution
 BOARD_SPACE_SIZE = 20
+
+DISPLAY_RESOLUTION_WIDTH = BOARD_SIZE_WIDTH * BOARD_SPACE_SIZE
+DISPLAY_RESOLUTION_HEIGHT = BOARD_SIZE_HEIGHT * BOARD_SPACE_SIZE
 
 PLAYER1_START_X = 12
 PLAYER1_START_Y = 12
 
 COBRA_START_DIRECTION = "Up"
+# Number of milliseconds between player movements.
+MOVEMENT_DELAY = 100
 
 SEGMENT_SIZE = 20
 
@@ -27,48 +35,79 @@ BLACK = (0x00, 0x00, 0x00)
 
 class board(object):
     foodOnBoard = False
+
     def __init__(self, width, height, spaceSize):
         self.width = width
         self.height = height
         self.spaceSize = spaceSize
-        self.spaces = [[0 for y in range(0, BOARD_SIZE_X)] for x in range(0, BOARD_SIZE_Y)]
-    class cobra(object):
-        numberOfSegments = 1
-        sizeOfSegments = SEGMENT_SIZE
-        posX = PLAYER1_START_X
-        posY = PLAYER1_START_Y
-        direction = COBRA_START_DIRECTION
-        segments = []
+        self.spaces = [[0 for y in range(0, BOARD_SIZE_HEIGHT)] for x in range(0, BOARD_SIZE_WIDTH)]
 
-class segment(object):
-    def __init__(self, posX, posY):
+    def create_food(self):
+        while self.foodOnBoard == False:
+            x = random.randint(0, BOARD_SIZE_WIDTH - 1)
+            y = random.randint(0, BOARD_SIZE_HEIGHT - 1)
+            if self.spaces[x][y] == EMPTY:
+                self.spaces[x][y] = FOOD
+                self.foodOnBoard = True
+
+    def draw_board(self, screen):
+        for y in range(0, BOARD_SIZE_HEIGHT):
+            for x in range(0, BOARD_SIZE_WIDTH):
+                drawX = x * BOARD_SPACE_SIZE
+                drawY = y * BOARD_SPACE_SIZE
+                if self.spaces[x][y] == COBRA:
+                    pygame.draw.rect(screen, RED, pygame.Rect(drawX, drawY, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE))
+                elif self.spaces[x][y] == FOOD:
+                    pygame.draw.rect(screen, YELLOW, pygame.Rect(drawX, drawY, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE))
+                elif self.spaces[x][y] == EMPTY:
+                    pygame.draw.rect(screen, BLACK, pygame.Rect(drawX, drawY, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE))
+    
+class cobra(object):
+    numberOfSegments = 1
+    sizeOfSegments = SEGMENT_SIZE
+    segments = []
+    
+    def __init__(self, posX, posY, direction):
         self.posX = posX
         self.posY = posY
+        self.direction = direction
+        
+    class segment(object):
+        def __init__(self, posX, posY):
+            self.posX = posX
+            self.posY = posY
 
-def draw_board(board):
-    for y in range(0, BOARD_SIZE_Y):
-        for x in range(0, BOARD_SIZE_X):
-            drawX = x * BOARD_SPACE_SIZE
-            drawY = y * BOARD_SPACE_SIZE
-            if board.spaces[x][y] == COBRA:
-                pygame.draw.rect(screen, RED, pygame.Rect(drawX, drawY, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE))
-            elif board.spaces[x][y] == FOOD:
-                pygame.draw.rect(screen, YELLOW, pygame.Rect(drawX, drawY, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE))
-            elif board.spaces[x][y] == EMPTY:
-                pygame.draw.rect(screen, BLACK, pygame.Rect(drawX, drawY, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE))
+    def add_segment(self, segment):
+        self.segments.append(segment)
 
-def create_food(board):
-    x = random.randint(0, BOARD_SIZE_X - 1)
-    y = random.randint(0, BOARD_SIZE_Y - 1)
-    board.spaces[x][y] = FOOD
+    def move(self):
+        if self.direction == "Up":
+            self.posY -= 1
+        if self.direction == "Down":
+            self.posY += 1
+        if self.direction == "Left":
+            self.posX -= 1
+        if self.direction == "Right":
+            self.posX += 1    
 
-def update_segments(board):
-    posX = board.cobra.posX
-    posY = board.cobra.posY
-    if board.spaces[posX][board.cobra.posY] == FOOD:
-        board.cobra.segments.append(segment(board.cobra.posX, board.cobra.posY))
+def check_collision(board, cobra):
+    if (cobra.posX < 0) or (cobra.posY < 0) or (cobra.posX > BOARD_SIZE_WIDTH - 1)\
+    or (cobra.posY > BOARD_SIZE_HEIGHT - 1) or (board.spaces[cobra.posX][cobra.posY] == COBRA):
+        collision = True
+    else:
+        collision = False
+
+    return collision
+
+def update_segments(board, cobra):
+    posX = cobra.posX
+    posY = cobra.posY
+
+    if board.spaces[posX][posY] == FOOD:
+        cobra.add_segment(cobra.segment(posX, posY))
         board.foodOnBoard = False
-    for seg in board.cobra.segments:
+
+    for seg in cobra.segments:
         prevPosX = seg.posX
         prevPosY = seg.posY
         board.spaces[prevPosX][prevPosY] = EMPTY
@@ -78,60 +117,56 @@ def update_segments(board):
         posX = prevPosX
         posY = prevPosY
 
-def move_cobra(board):
-    collision = False
-    if board.cobra.direction == "Up":
-        board.cobra.posY -= 1
-    if board.cobra.direction == "Down":
-        board.cobra.posY += 1
-    if board.cobra.direction == "Left":
-        board.cobra.posX -= 1
-    if board.cobra.direction == "Right":
-        board.cobra.posX += 1
-    if (board.cobra.posX < 0) or (board.cobra.posY < 0) or (board.cobra.posX > BOARD_SIZE_X - 1)\
-    or (board.cobra.posX > BOARD_SIZE_Y - 1) or (board.spaces[board.cobra.posX][board.cobra.posY] == COBRA):
-        collision = True
-    update_segments(board)
-    return collision
+def main():
+    # Set board size, cobra starting position, create first cobra segment, and initialize display
+    board1 = board(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, BOARD_SPACE_SIZE)
+    cobra1 = cobra(PLAYER1_START_X, PLAYER1_START_Y, COBRA_START_DIRECTION)
+    cobra1.add_segment(cobra.segment(cobra1.posX, cobra1.posY))
 
-def update_board(board):
-    board.spaces[board.cobra.posX][board.cobra.posY] = EMPTY
-    collision = move_cobra(board)
-    return collision
+    pygame.init()
 
-# Set board size, cobra starting position, create first cobra segment, and initialize display
-board1 = board(BOARD_SIZE_X, BOARD_SIZE_Y, BOARD_SPACE_SIZE)
-#board1.cobra.posX = PLAYER1_START_X
-#board1.cobra.posY = PLAYER1_START_Y
-board1.cobra.segments.append(segment(board1.cobra.posX, board1.cobra.posY))
+    screen = pygame.display.set_mode((DISPLAY_RESOLUTION_WIDTH, DISPLAY_RESOLUTION_HEIGHT))
 
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
+    gameRunning = True
+    paused = False
 
-gameRunning = True
+    while gameRunning == True:
 
-while gameRunning == True:
-    if not board1.foodOnBoard:
-        create_food(board1)
-        board1.foodOnBoard = True
+        update_segments(board1, cobra1)
+        board1.draw_board(screen)
+        pygame.display.update()
 
-    collision = update_board(board1)
-    if collision == True:
-        gameRunning = False
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    cobra1.direction = "Up"
+                if event.key == pygame.K_DOWN:
+                    cobra1.direction = "Down"
+                if event.key == pygame.K_LEFT:
+                    cobra1.direction = "Left"
+                if event.key == pygame.K_RIGHT:
+                    cobra1.direction = "Right"
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                if event.key == pygame.K_SPACE:
+                    if not paused:
+                        paused = True
+                    else:
+                        paused = False
 
-    draw_board(board1)
-    pygame.display.update()
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                board1.cobra.direction = "Up"
-            if event.key == pygame.K_DOWN:
-                board1.cobra.direction = "Down"
-            if event.key == pygame.K_LEFT:
-                board1.cobra.direction = "Left"
-            if event.key == pygame.K_RIGHT:
-                board1.cobra.direction = "Right"
-    pygame.time.wait(100)
+        if not paused:
+            cobra1.move()
+            collision = check_collision(board1, cobra1)
+
+            if not board1.foodOnBoard:
+                board1.create_food()
+
+            if collision == True:
+                gameRunning = False
+
+            pygame.time.wait(MOVEMENT_DELAY)
+
+main()
